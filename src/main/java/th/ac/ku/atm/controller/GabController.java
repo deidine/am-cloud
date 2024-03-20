aa88d796404e6169cfd15987c18506ecfdf4b41f
deidine <deidinemycours@gmail.com>
2024-03-20 14:14:55 +0000

    cloud

--- src/main/java/th/ac/ku/atm/controller/GabController.java
+++ src/main/java/th/ac/ku/atm/controller/GabController.java
@@ -1,6 +1,8 @@
 package th.ac.ku.atm.controller;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,9 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import th.ac.ku.atm.model.Carte;
 import th.ac.ku.atm.model.Compte;
 import th.ac.ku.atm.model.Machine;
+import th.ac.ku.atm.model.Transaction;
 import th.ac.ku.atm.model.User;
 import th.ac.ku.atm.repository.CarteRepository;
 import th.ac.ku.atm.repository.CompteRepository;
+import th.ac.ku.atm.repository.MachineRepository;
+import th.ac.ku.atm.repository.TransactionRepository;
+import th.ac.ku.atm.service.CarteDao;
+import th.ac.ku.atm.service.MachineDao;
 
 @Controller
 @RequestMapping("/atm")
@@ -27,6 +34,9 @@ public class GabController {
     @Autowired
     CompteRepository comteRepo;
 
+	@Autowired
+	MachineRepository machrepository;
+	@Autowired TransactionRepository trensrepo;
     private int remainingAttempts = 3;
 
     @GetMapping("code")
@@ -52,6 +62,10 @@ public class GabController {
             model.addAttribute("carte", crt);
             model.addAttribute("user", user);
             model.addAttribute("compte", cmpt);
+            Machine mch=new Machine(); 
+            mch.setLogg(" L\'utilisateur "+user.getPhone()+"a entrer dans le machine numero compte "+cmpt.getNumCompte());
+             machrepository.save (mch);
+    
             return "WEB-INF/gab"; // Remplacez avec la page appropriée
         } else {
             // Si le code PIN est incorrect, décrémente le nombre d'essais restants
@@ -61,7 +75,6 @@ public class GabController {
             return "WEB-INF/code";
         }
     }
-
 
     @GetMapping("ticket")
     String printTicket(Model model) {
@@ -74,26 +87,43 @@ public class GabController {
         // Récupérer le compte et effectuer le retrait
         Compte cmpt = comteRepo.getById(Integer.parseInt(compteId));
         Double montant2 = Double.parseDouble(montant);
-
+        System.out.println(compteId + " deidine " + montant + " " + noCarte);
         // Vérifier si le solde est suffisant pour le retrait
         if (cmpt != null && cmpt.getSolde() >= montant2) {
             cmpt.setSolde(cmpt.getSolde() - montant2);
             comteRepo.save(cmpt);
 
+            String date = new SimpleDateFormat("yyyy/MM/dd").format(Calendar.getInstance().getTime());
+            String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+	 
+		String pin = noCarte;
+		  
+        Transaction tr=new Transaction();
+        tr.setInformation("l\'user "+cmpt.getUser()+" a faire une transaction dans le temps "+date);
+        tr.setUser(cmpt.getUser());
+        trensrepo.save(tr);
+
+
+
+
+	
             // Ajouter les attributs nécessaires pour l'impression du ticket
             model.addAttribute("carte", repository.getCartByPin(Integer.parseInt(noCarte)));
             model.addAttribute("user", cmpt.getUser());
             model.addAttribute("compte", cmpt);
+            model.addAttribute("date", date);
+            model.addAttribute("timeStamp", timeStamp);
             model.addAttribute("montant", montant2);
-
-            return "redirect:/atm/ticket"; // Rediriger vers la page ticket.html après le retrait
+            // User u=cmpt.getUser();
+            // u.getFirstName()
+            // u.getLastName()
+            // cmpt.getSolde()
+            return "WEB-INF/ticket"; // Rediriger vers la page ticket.html après le retrait
         } else {
             // Gérer le cas où le solde est insuffisant
             model.addAttribute("error", "true");
             return "WEB-INF/code";
         }
     }
-
-
 
 }
